ae30cf4c4cb0299b419c911a7c64dfdfad706eca
Sanket Bhoite <125543017+SB2157@users.noreply.github.com>
2025-11-25 14:07:50 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+  host: true,
+  hmr: {
+    overlay: false,
+  },
     proxy: {
       "/api": {
         target: "https://real-estate-backend-9oyt.onrender.com",
